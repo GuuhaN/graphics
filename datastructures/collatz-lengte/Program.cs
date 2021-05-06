34b2c44e38830cb5f003fcc0e2a324b905cc3180
Nigel Cheung <nigelcheung97@hotmail.com>
2021-05-06 02:49:40 +0200

    project created

---
+++ datastructures/collatz-lengte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace collatz_lengte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
