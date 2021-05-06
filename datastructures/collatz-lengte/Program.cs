500fac46f04c64547390e460335ad794a236684c
Nigel Cheung <nigelcheung97@hotmail.com>
2021-05-06 03:15:12 +0200

    collatz done

--- datastructures/collatz-lengte/Program.cs
+++ datastructures/collatz-lengte/Program.cs
@@ -4,9 +4,25 @@ namespace collatz_lengte
 {
     class Program
     {
+        static readonly int[] _testingNumbers = { 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculateCollatz(1);
+        }
+        
+        static void CalculateCollatz(int value)
+        {
+            int collatzNumber = value;
+            Console.WriteLine(collatzNumber);
+            while (collatzNumber != 1)
+            {
+                if (collatzNumber % 2 != 0) // Odd number
+                    collatzNumber = (collatzNumber * 3) + 1;
+                else // Even number
+                    collatzNumber = collatzNumber / 2;
+
+                Console.WriteLine(collatzNumber);
+            }
         }
     }
 }
