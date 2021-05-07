a3cc185e9b3139e6a09211184373ee6a6714515d
Nigel Cheung <nigelcheung97@hotmail.com>
2021-05-07 18:13:13 +0200

    Fixed to the likes of domjudge

--- datastructures/collatz-lengte/Program.cs
+++ datastructures/collatz-lengte/Program.cs
@@ -4,24 +4,34 @@ namespace collatz_lengte
 {
     class Program
     {
-        static readonly int[] _testingNumbers = { 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 };
         static void Main(string[] args)
         {
-            CalculateCollatz(1);
+            long input = Convert.ToUInt32(Console.ReadLine());
+            long input2 = Convert.ToUInt32(Console.ReadLine());
+            long input3 = Convert.ToUInt32(Console.ReadLine());
+            long input4 = Convert.ToUInt32(Console.ReadLine());
+            CalculateCollatz(input, input2, input3, input4); // Take 4 input values
         }
         
-        static void CalculateCollatz(int value)
+        static void CalculateCollatz(long value1, long value2, long value3, long value4)
         {
-            int collatzNumber = value;
-            Console.WriteLine(collatzNumber);
-            while (collatzNumber != 1)
+            long[] values = { value1, value2, value3, value4 }; // Using long instead of int, because one of the tests exceeds 2.1billion (max number of an int and even exceeds from unsigned int)
+            foreach (long number in values)
             {
-                if (collatzNumber % 2 != 0) // Odd number
-                    collatzNumber = (collatzNumber * 3) + 1;
-                else // Even number
-                    collatzNumber = collatzNumber / 2;
+                long collatzNumber = number;
+                // string collatz = number.ToString(); Enable this to visualize it, but having this will affect performance
+                int steps = 0;
+                while (collatzNumber != 1)
+                {
+                    if (collatzNumber % 2 != 0) // Odd number
+                        collatzNumber = (collatzNumber * 3) + 1;
+                    else // Even number
+                        collatzNumber = collatzNumber / 2;
 
-                Console.WriteLine(collatzNumber);
+                    //collatz += "->" + collatzNumber; Enable this to visualize it, but having this will affect performance
+                    steps += 1;
+                }
+                Console.WriteLine(steps);
             }
         }
     }
