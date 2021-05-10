561ebab7e14ece8f887fad98e8e053c78868b3b7
Nigel Cheung <nigelcheung97@hotmail.com>
2021-05-10 11:06:07 +0200

    inpakken programma aangemaakt

---
+++ datastructures/inpakken/inpakken/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inpakken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Inpakken(int n, int m)
+        {
+
+        }
+    }
+}
