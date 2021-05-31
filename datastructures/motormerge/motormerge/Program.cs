d8acafa1cba101a1e7bc68a16b20fa64fa0438f0
Nigel Cheung <nigelcheung97@hotmail.com>
2021-05-31 17:36:37 +0200

    updated filter

--- datastructures/motormerge/motormerge/Program.cs
+++ datastructures/motormerge/motormerge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace motormerge
@@ -22,14 +23,16 @@ namespace motormerge
                 motors.Add((motorInput[0], Convert.ToInt32(motorInput[1]), Convert.ToInt32(motorInput[2]), Convert.ToInt32(motorInput[3])));
             }
 
-            var mergeSortedList = MergeSort(motors);
+            var mergeSortedList = MergeSort(motors, Filter.Snelheid);
+            mergeSortedList.Reverse();
+            Console.WriteLine(motors.Count);
             foreach (var item in mergeSortedList)
             {
                 Console.WriteLine(item.Item1);
             }
         }
 
-        static List<(string, int, int, int)> MergeSort(List<(string, int, int, int)> mergeMotors)
+        static List<(string, int, int, int)> MergeSort(List<(string, int, int, int)> mergeMotors, Filter filter)
         {
             if (mergeMotors.Count <= 1)
                 return mergeMotors;
@@ -48,18 +51,35 @@ namespace motormerge
                 i++;
             }
 
-            tempLeft = MergeSort(tempLeft);
-            tempRight = MergeSort(tempRight);
+            tempLeft = MergeSort(tempLeft, filter);
+            tempRight = MergeSort(tempRight, filter);
 
-            return Merge(tempLeft, tempRight);
+            return Merge(tempLeft, tempRight, filter);
         }
 
-        static List<(string, int, int, int)> Merge(List<(string, int, int, int)> left, List<(string, int, int, int)> right)
+        static bool MergeByRules(Filter filter, (string, int, int, int) a, (string, int, int, int) b)
+        {
+            if (filter == Filter.Snelheid)
+            {
+                int[] aSplit = a.Item3.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+                int[] bSplit = b.Item3.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+                if (aSplit[0] <= bSplit[0] && aSplit[1] <= bSplit[1] && aSplit[2] <= bSplit[2])
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return a.Item4 <= b.Item4;
+            }
+        }
+
+        static List<(string, int, int, int)> Merge(List<(string, int, int, int)> left, List<(string, int, int, int)> right, Filter filter)
         {
             List<(string, int, int, int)> result = new List<(string, int, int, int)>();
             while (left.Count > 0 && right.Count > 0)
             {
-                if (left[0].Item3 <= right[0].Item3) // Sorteert nu alleen op snelheid (SORTEER REGELS HIER)
+                if (MergeByRules(filter, left[0], right[0])) // Sorteert nu alleen op snelheid (SORTEER REGELS HIER)
                 {
                     result.Add(left[0]);
                     left.RemoveAt(0);
