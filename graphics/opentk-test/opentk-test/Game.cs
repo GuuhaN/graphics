0c02fa6888faba91f4eaebbd9429fb86b227e597
Nigel Cheung <nigelcheung97@hotmail.com>
2021-06-04 20:57:18 +0200

    opentk setup

---
+++ graphics/opentk-test/opentk-test/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Desktop;
+
+namespace opentk_test
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, NativeWindowSettings.Default) { 
+        
+        }
+    }
+}
