1f07ea303bd67a1568100581a941e500daa17fd4
Nigel Cheung <nigelcheung97@hotmail.com>
2021-06-11 23:15:26 +0200

    first implementation

--- datastructures/nemo/nemo/Program.cs
+++ datastructures/nemo/nemo/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace nemo
 {
     class Program
     {
-        static Dictionary<int, byte> animalsInCart = new Dictionary<int, byte>();
-        int maxWeight = 0;
+        static int[] bedreigdeAnimals;
+        static Dictionary<int, int> configurations = new Dictionary<int, int>();
+
+        static int maxAnimalTypes = 0;
+        static int maxCages = 0;
+        static int maxWeight = 0;
+
+        static int huidigeAantal = 0;
+        static int huidigeHokken = 0;
+        static int huidigeGewicht = 0;
+
+        static uint animalsInCart = 0;
+        static uint animalsInCartTemp = 0;
+
         static void Main(string[] args)
         {
             ReadInput();
@@ -14,13 +27,21 @@ namespace nemo
 
         static void ReadInput()
         {
-            string firstLine = Console.ReadLine();
-            string animalLine = Console.ReadLine().Replace(" ", "");
-            Console.WriteLine(Convert.ToUInt32(animalLine));
+            string[] firstLine = Console.ReadLine().Split();
+            string[] animalLine = Console.ReadLine().Split();
+
+            maxAnimalTypes = Convert.ToInt32(firstLine[0]);
+            maxCages = Convert.ToInt32(firstLine[1]);
+            maxWeight = Convert.ToInt32(firstLine[2]);
+
+            bedreigdeAnimals = new int[maxAnimalTypes];
+            for (int i = 0; i < maxAnimalTypes; i++)
+                bedreigdeAnimals[i] = Convert.ToInt32(animalLine[i]);
+
             while(true)
             {
                 string commandLine = Console.ReadLine();
-                if (commandLine[0].Equals("e"))
+                if (commandLine == "e")
                     break;
 
                 SetRules(commandLine);
@@ -33,17 +54,71 @@ namespace nemo
             if(split[0].Equals("p"))
             {
                 int animalKey = Convert.ToInt32(split[1]);
-                Console.WriteLine("COMMAND P - ANIMALWEIGHT: " + animalKey);
+                if ((animalsInCart & (1 << animalKey)) > 0)
+                {
+                    Console.WriteLine("WEIGER {0}", animalKey);
+                }
+                else
+                {
+                    if (bedreigdeAnimals[animalKey] + huidigeGewicht <= maxWeight &&
+                        huidigeHokken + 1 <= maxCages)
+                    {
+                        animalsInCart |= (uint)(1 << animalKey);
+                        huidigeGewicht += bedreigdeAnimals[animalKey];
+                        huidigeAantal++;
+                        huidigeHokken++;
+                    }
+                    else if(bedreigdeAnimals[animalKey] + huidigeGewicht > maxWeight ||
+                        huidigeHokken + 1 > maxCages)
+                    {
+                        Console.WriteLine("WEIGER {0}", animalKey);
+                    }
+                }
+                animalsInCartTemp |= (uint)(1 << animalKey);
+                if (configurations.ContainsKey((int)animalsInCartTemp))
+                    configurations[(int)animalsInCartTemp] += 1;
+                else
+                    configurations.Add((int)animalsInCartTemp, 1);
 
             }
             if (split[0].Equals("l"))
             {
                 int animalKey = Convert.ToInt32(split[1]);
-                Console.WriteLine("COMMAND L - ANIMALWEIGHT: " + animalKey);
-                //if (!animals[animalKey])
-                //    return;
 
-                //animals.Remove(animalKey);
+                for (int i = 0; i < maxAnimalTypes; i++)
+                {
+                    if ((animalsInCart & (1 << i)) > 0)
+                    {
+                        animalsInCart &= (uint)~(1 << animalKey);
+                        huidigeGewicht -= bedreigdeAnimals[animalKey];
+                        huidigeAantal--;
+                        huidigeHokken--;
+                    }
+                }
+                animalsInCartTemp &= (uint)~(1 << animalKey);
+                if (configurations.ContainsKey((int)animalsInCartTemp))
+                    configurations[(int)animalsInCartTemp] += 1;
+            }
+            if (split[0].Equals("a"))
+            {
+                string test = "";
+                for (int i = 0; i < maxAnimalTypes; i++)
+                {
+                    if ((animalsInCart & (1 << i)) > 0)
+                    {
+                        test += i + " ";
+                    }
+                }
+                Console.WriteLine(test);
+            }
+            if (split[0].Equals("c"))
+            {
+                if(configurations.ContainsKey((int)animalsInCart))
+                    Console.WriteLine("HERHAALD " + configurations[(int)animalsInCart]);
+            }
+            if (split[0].Equals("q"))
+            {
+                Console.WriteLine("Aantal " + huidigeAantal + " Gewicht " + huidigeGewicht);
             }
         }
     }
