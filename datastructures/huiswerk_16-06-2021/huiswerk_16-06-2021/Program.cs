8a7daf931c0504bb76e3b5a8ae30e06ed3e90511
Nigel Cheung <nigelcheung97@hotmail.com>
2021-06-16 15:23:24 +0200

    added comments

--- datastructures/huiswerk_16-06-2021/huiswerk_16-06-2021/Program.cs
+++ datastructures/huiswerk_16-06-2021/huiswerk_16-06-2021/Program.cs
@@ -6,6 +6,7 @@ namespace huiswerk_16_06_2021
 {
     class Program
     {
+        // Initialize the game
         static List<string> A = new List<string>() { "aaa", "bbb", "ccc", "ddd"};
         static string r = "";
         static int counter = 0;
@@ -15,8 +16,10 @@ namespace huiswerk_16_06_2021
             Console.WriteLine(r);
         }
 
+        // Concat the string
         static void ConcatString(List<string> a, List<string> b)
         {
+            // Check if is the first half
             if(counter < A.Count/2)
             {
                 r += a[0] + b[0];
@@ -25,12 +28,14 @@ namespace huiswerk_16_06_2021
 
         }
 
+        // Basic implementation of the MergeSort, but to divide the arrays in recursion
         static List<string> Merge(List<string> a)
         {
             if (a.Count <= 1)
                 return a;
 
             List<string> tempLeft = new List<string>(), tempRight = new List<string>();
+            // First half goes to the left, and second half goes to the right
             for (int i = 0; i < a.Count; i++)
             {
                 if (i < a.Count / 2)
@@ -39,6 +44,7 @@ namespace huiswerk_16_06_2021
                     tempRight.Add(a[i]);
             }
 
+            // Recursion of both sides and do it till it's the last one.
             if(tempLeft.Count > 1 && tempRight.Count > 1)
             {
                 tempLeft = Merge(tempLeft);
