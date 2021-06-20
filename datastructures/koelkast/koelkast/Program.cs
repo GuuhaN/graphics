e72e1d836dcdac0c46c8b75cc376bf9ddcbf0f94
Nigel Cheung <nigelcheung97@hotmail.com>
2021-06-20 23:47:45 +0200

    setup project and read input

---
+++ datastructures/koelkast/koelkast/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace koelkast
+{
+    class Program
+    {
+        static char[,] map;
+        enum OutputForm { Length, Path };
+        static OutputForm outputForm;
+        static void Main(string[] args)
+        {
+            ReadMap();
+        }
+
+        static void ReadMap()
+        {
+            string[] firstInputs = Console.ReadLine().Split();
+            map = new char[Convert.ToInt32(firstInputs[0]), Convert.ToInt32(firstInputs[1])];
+            outputForm = firstInputs[2] == "L" ? OutputForm.Length : OutputForm.Path;
+            for (int i = 0; i < Convert.ToInt32(firstInputs[0]); i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < Convert.ToInt32(firstInputs[1]); j++)
+                {
+                    map[i, j] = input[j];
+                }
+            }
+        }
+    }
+}
