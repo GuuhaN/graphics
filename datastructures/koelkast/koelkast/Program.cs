ac26f47db7a7db80b95dfc160de6477314e92efa
Nigel Cheung <nigelcheung97@hotmail.com>
2021-06-26 03:55:58 +0200

    properly fill the map

--- datastructures/koelkast/koelkast/Program.cs
+++ datastructures/koelkast/koelkast/Program.cs
@@ -5,6 +5,7 @@ namespace koelkast
     class Program
     {
         static char[,] map;
+        static int mapX, mapY;
         enum OutputForm { Length, Path };
         static OutputForm outputForm;
         static void Main(string[] args)
@@ -15,14 +16,16 @@ namespace koelkast
         static void ReadMap()
         {
             string[] firstInputs = Console.ReadLine().Split();
-            map = new char[Convert.ToInt32(firstInputs[0]), Convert.ToInt32(firstInputs[1])];
+            mapX = Convert.ToInt32(firstInputs[0]);
+            mapY = Convert.ToInt32(firstInputs[1]);
+            map = new char[mapX, mapY];
             outputForm = firstInputs[2] == "L" ? OutputForm.Length : OutputForm.Path;
-            for (int i = 0; i < Convert.ToInt32(firstInputs[0]); i++)
+            for (int i = 0; i < mapY; i++)
             {
                 char[] input = Console.ReadLine().ToCharArray();
-                for (int j = 0; j < Convert.ToInt32(firstInputs[1]); j++)
+                for (int j = 0; j < mapX; j++)
                 {
-                    map[i, j] = input[j];
+                    map[j, i] = input[j];
                 }
             }
         }
